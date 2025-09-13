e95fca3eebab0cc7cbdd84ddce68245b31174f75
Alihaf8 <slashkill614@gmail.com>
2025-09-13 23:55:55 +0300

    #

--- quotescraper/options/clear_txt_file.py
+++ quotescraper/options/clear_txt_file.py
@@ -4,7 +4,7 @@ import os
 
 def clear_txt_file():
     for _ in range(10):
-        if not os.path.basename(os.getcwd()) == "goodreads":
+        if not "goodreads" in os.path.basename(os.getcwd()).lower():
             os.chdir('..')
 
     if not os.path.exists(QUOTE_PATH):
